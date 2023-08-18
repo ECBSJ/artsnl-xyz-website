700a3fc30ad8dc784e566999c9db52f20a9cde5b
ECBSJ <crypto67sauce@gmail.com>
2023-08-18 08:20:34 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <a href="https://artsnl.app" target="_blank">
+          <img className="logo" src="https://i.imgur.com/5fv5p7q.png" alt="artsnl-icon" />
+        </a>
+        <h1>ARTSNL</h1>
+      </div>
+
+      <p style={{ color: "gray" }}>
+        Main website for <code>artsnl.xyz</code> is still under construction.
+      </p>
+      <p>Try our beta app by clicking the purple icon above!</p>
+    </>
+  )
+}
+
+export default App
